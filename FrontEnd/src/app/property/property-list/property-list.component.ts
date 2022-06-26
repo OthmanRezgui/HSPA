f709f0eeb3310c43a549cf89a29e5e61ecf7bf70
othman rezgui <othman.rezgui.dotnet@gmail.com>
2022-06-26 14:02:16 +0200

    Add service and API call

--- FrontEnd/src/app/property/property-list/property-list.component.ts
+++ FrontEnd/src/app/property/property-list/property-list.component.ts
@@ -1,56 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
-  styleUrls: ['./property-list.component.css']
+  styleUrls: ['./property-list.component.css'],
 })
 export class PropertyListComponent implements OnInit {
+  PropertyArray: any;
 
-  PropertyArray: Array<any> = [{
-    "Id":1,
-    "Type":"House",
-    "Price": 300,
-    "Name" :"House Antony"
-  },
-  {
-    "Id":2,
-    "Type":"House",
-    "Price": 500,
-    "Name" :"Clamart"
-  },
-  {
-    "Id":3,
-    "Type":"Appartement",
-    "Price": 783,
-    "Name" :"Saint germain en laye"
-  },
-  {
-    "Id":4,
-    "Type":"Appartement",
-    "Price": 1000,
-    "Name" :"Rueil Malmaison"
-  },
-  {
-    "Id":5,
-    "Type":"Appartement",
-    "Price": 1000,
-    "Name" :"Houilles"
-  },
-  {
-    "Id":6,
-    "Type":"Appartement",
-    "Price": 1000,
-    "Name" :"Paris"
-  }
-
-
-
-]
-
-  constructor() { }
+  constructor(private housingService: HousingService) {}
 
   ngOnInit(): void {
+    this.housingService.getAllProperties().subscribe((data) => {
+      console.log(data);
+      this.PropertyArray = data;
+    },
+    error=> console.log(error));
   }
-
 }
