4114d95b148815e827085538516a433b6f195b4c
othman rezgui <othman.rezgui.dotnet@gmail.com>
2022-06-26 14:23:22 +0200

    Add pipes to Http call

---
+++ FrontEnd/src/app/property/IProperty.interface.ts
@@ -0,0 +1,10 @@
+export interface IProperty
+{
+
+Id: number;
+Name : string;
+Price: number;
+Type:string;
+
+
+}
--- FrontEnd/src/app/services/housing.service.ts
+++ FrontEnd/src/app/services/housing.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import { IProperty } from '../property/IProperty.interface';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -7,9 +10,23 @@ export class HousingService {
 
 constructor(private http:HttpClient) { }
 
-getAllProperties()
+getAllProperties() :Observable<IProperty[]>
 {
-  return this.http.get('data/properties.json');
+  return this.http.get('data/properties.json').pipe(
+    map(data => {
+
+      const propertiesArray: Array<IProperty>= [];
+      for (const id in data)
+      {
+        if(data.hasOwnProperty(id))
+        {
+        propertiesArray.push(data[id]);
+        }
+      }
+
+      return propertiesArray;
+    })
+  );
 }
 
 }
