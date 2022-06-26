d738303025b4a63949721e0590b2c6ac646fdadd
othman rezgui <othman.rezgui.dotnet@gmail.com>
2022-06-26 14:34:07 +0200

    Add Routing Part1

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -6,6 +6,22 @@ import { PropertyCardComponent } from './property/property-card/property-card.co
 import { PropertyListComponent } from './property/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HousingService } from './services/housing.service';
+import { AddPropertyComponent } from './property/add-property/add-property.component';
+ import { Routes, RouterModule } from '@angular/router';
+
+ const appRoutes: Routes =
+ [
+  {
+    path:"add-property",
+    component: AddPropertyComponent
+  },
+  {
+    path:"",
+    component: PropertyListComponent
+  }
+
+
+ ]
 
 
 @NgModule({
@@ -13,11 +29,13 @@ import { HousingService } from './services/housing.service';
     AppComponent,
     PropertyCardComponent,
     PropertyListComponent,
-      NavBarComponent
+    NavBarComponent,
+    AddPropertyComponent
    ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     HousingService
