6c0fa870f1b171abc979704125c2c2d7c108e23f
othman rezgui <othman.rezgui.dotnet@gmail.com>
2022-07-02 16:39:44 +0200

    Add form validation with reactive forms

--- FrontEnd/src/app/user/user-register/user-register.component.ts
+++ FrontEnd/src/app/user/user-register/user-register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-register',
@@ -13,17 +13,38 @@ export class UserRegisterComponent implements OnInit {
 
   ngOnInit() {
     this.registrationForm = new FormGroup({
-    userName: new FormControl('Mark', Validators.required),
+    userName: new FormControl(null, Validators.required),
     email: new FormControl(null,[Validators.required, Validators.email]),
     password: new FormControl(null,[Validators.required,Validators.minLength(8)]),
     confirmPassword: new FormControl(null,[Validators.required]),
     mobile: new FormControl(null,[Validators.required, Validators.maxLength(10)])
-    });
+    },this.passwordMatchingValidator);
   }
 
+  passwordMatchingValidator(fc: AbstractControl): ValidationErrors | null {
+    return fc.get('password')?.value === fc.get('confirmPassword')?.value ? null :
+      { notmatched: true }
+  };
   onSubmit()
   {
     console.log(this.registrationForm);
+  }
+
+  get userName()
+  {
+    return this.registrationForm.get('userName') as FormControl;
+  }
+  get email() {
+    return this.registrationForm.get('email') as FormControl;
+  }
+  get password() {
+    return this.registrationForm.get('password') as FormControl;
+  }
+  get confirmPassword() {
+    return this.registrationForm.get('confirmPassword') as FormControl;
+  }
+  get mobile() {
+    return this.registrationForm.get('mobile') as FormControl;
   }
 
 }
